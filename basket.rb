1296676fad7f6c2616e7e06b54f1e0766efa3af3
Demeugaliyev <m.demeugaliev@gmail.com>
2018-08-03 19:38:57 +0600

    sixth task

---
+++ basket.rb
@@ -0,0 +1,36 @@
+name = 
+
+hash = Hash.new
+
+until name == "stop"
+	puts "Enter name"
+	name = gets.chomp
+
+	if name == "stop"
+		break
+	end
+
+	puts "Enter price"
+	price = gets.chomp.to_i
+	
+	puts "Enter quantity"
+	quantity = gets.chomp.to_f
+
+	sum_hash = {price => quantity}
+	hash[name] = sum_hash
+end
+
+puts hash
+
+all_sum = 0
+
+hash.each do |key, value|
+	sum = 0
+	value.each do |price_of, quantity_of|
+		sum = price_of*quantity_of
+		all_sum += sum
+	end
+	puts "#{key} sum is #{sum}"
+end
+
+puts all_sum
