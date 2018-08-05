f5324e5e7e7b44a65419721b4930371681c02902
Demeugaliyev <m.demeugaliev@gmail.com>
2018-08-05 18:22:49 +0600

    refactoring

--- months_with_30days.rb
+++ months_with_30days.rb
@@ -1,16 +1,16 @@
 year = {
-	jan: 31,
-	feb: 28,
-	mar: 31,
-	apr: 30,
-	may: 31,
-	jun: 30,
-	jul: 31,
-	aug: 31,
-	sep: 30,
-	oct: 31,
-	nov: 30,
-	dec: 31
+  jan: 31,
+  feb: 28,
+  mar: 31,
+  apr: 30,
+  may: 31,
+  jun: 30,
+  jul: 31,
+  aug: 31,
+  sep: 30,
+  oct: 31,
+  nov: 30,
+  dec: 31
 }
 
 year.each { |month, day| puts month if day == 30}
