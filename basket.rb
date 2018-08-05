7ab14f14e7d822b1c2ca59114169b0f54c158d0c
Demeugaliyev Mirosh <39377563+Demeugaliyev@users.noreply.github.com>
2018-08-06 00:53:37 +0600

    refactored

--- basket.rb
+++ basket.rb
@@ -1,23 +1,21 @@
-name = 
+name = ""
 
-hash = Hash.new
+hash = {}
 
 until name == "stop"
-	puts "Enter name"
-	name = gets.chomp
+  puts "Enter name"
+  name = gets.chomp
 
-	if name == "stop"
-		break
-	end
-
-	puts "Enter price"
-	price = gets.chomp.to_i
+  break if name == "stop"
 	
-	puts "Enter quantity"
-	quantity = gets.chomp.to_f
+  puts "Enter price"
+  price = gets.chomp.to_i
+	
+  puts "Enter quantity"
+  quantity = gets.chomp.to_f
 
-	sum_hash = {price => quantity}
-	hash[name] = sum_hash
+  sum_hash = {price => quantity}
+  hash[name] = sum_hash
 end
 
 puts hash
@@ -25,12 +23,12 @@ puts hash
 all_sum = 0
 
 hash.each do |key, value|
-	sum = 0
-	value.each do |price_of, quantity_of|
-		sum = price_of*quantity_of
-		all_sum += sum
-	end
-	puts "#{key} sum is #{sum}"
+  sum = 0
+  value.each do |price_of, quantity_of|
+    sum = price_of * quantity_of
+    all_sum += sum
+  end
+  puts "#{key} sum is #{sum}"
 end
 
 puts all_sum
