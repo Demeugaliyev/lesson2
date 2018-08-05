3d5655796f519083f3ae481472948a04c95fcd34
Demeugaliyev Mirosh <39377563+Demeugaliyev@users.noreply.github.com>
2018-08-06 00:43:56 +0600

    refactored

--- randomday.rb
+++ randomday.rb
@@ -10,13 +10,13 @@ year = gets.chomp.to_i
 which_day = 0
 
 if year % 400 == 0
-	leap_day = 29
+  leap_day = 29
 elsif year % 100 == 0
-	leap_day = 28
+  leap_day = 28
 elsif year % 4 == 0
-	leap_day = 29
+  leap_day = 29
 else
-	leap_day = 28
+  leap_day = 28
 end
 
 months = [31,leap_day, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -25,11 +25,11 @@ month -= 2
 
 
 if month < 0
-	which_day = 0
+  which_day = 0
 else
-	months[0..month].each do |mon|
-		which_day += mon
-	end
+  months[0..month].each do |mon|
+    which_day += mon
+  end
 end
 
 which_day += day
