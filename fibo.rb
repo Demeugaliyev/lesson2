03751a34ff3693ebf23fb0a3f0c9a7d9cc81c87c
Demeugaliyev Mirosh <39377563+Demeugaliyev@users.noreply.github.com>
2018-08-05 23:13:01 +0600

    refactored

--- fibo.rb
+++ fibo.rb
@@ -1,11 +1,7 @@
-arr = []
+arr = [0]
+push_element = 1
 
-arr.push(0, 1)
-
-while arr.last < 100
-	arr.push(arr[-1] + arr[-2])
-end
-
-if arr.last > 100
-	arr.delete_at(-1)
+while push_element < 100
+  arr.push(push_element)
+  push_element = arr[-1] + arr[-2]
 end
